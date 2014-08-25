5bd3cf02a49d386e1fab0d382a82c7804aafd32c
Jonas Lundberg <jonas@5monkeys.se>
2014-08-25 10:04:21 +0200

    Add version setting to elasticsearch

--- blues/elasticsearch.py
+++ blues/elasticsearch.py
@@ -19,7 +19,8 @@ def install():
         debian.debconf_set_selections('shared/accepted-oracle-license-v1-1 select true',
                                       'shared/accepted-oracle-license-v1-1 seen true')
         # Install elastic search
-        repository = 'deb http://packages.elasticsearch.org/elasticsearch/1.0/debian stable main'
+        version = blueprint.get('version', '1.0')
+        repository = 'deb http://packages.elasticsearch.org/elasticsearch/{0}/debian stable main'.format(version)
         info('Adding key for {0}'.format(repository))
         debian.add_apt_key('http://packages.elasticsearch.org/GPG-KEY-elasticsearch')
         files.append('/etc/apt/sources.list', repository, shell=True)
