07c6c41f31a3e4f210fb01dfe696108ad13ddfaa
Jonas Lundberg <jonas@5monkeys.se>
2014-10-08 16:59:34 +0200

    Python

---
+++ blues/python.py
@@ -0,0 +1,26 @@
+from fabric.decorators import task
+
+from refabric.api import run, info
+from refabric.context_managers import sudo
+from refabric.contrib import debian
+
+
+@task
+def setup():
+    install()
+
+
+@task
+def upgrade():
+    raise NotImplementedError
+
+
+def install():
+    with sudo():
+        debian.apt_get('install', 'python-dev', 'python-setuptools')
+        run('easy_install -0 pip')
+
+
+def pip(command, *options):
+    info('Running pip {}', command)
+    run('pip {} {}'.format(command, ' '.join(options)))
