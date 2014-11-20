d486fd0e64f64ef3fc1f2e1a46fdfa62c4639fc2
Jonas Lundberg <jonas@5monkeys.se>
2014-11-20 14:43:02 +0100

    Add missing application.reload task

--- blues/application/__init__.py
+++ blues/application/__init__.py
@@ -2,4 +2,4 @@ from refabric.contrib import blueprints
 blueprint = blueprints.get(__name__)
 
 from .tasks import *
-__all__ = ['setup', 'upgrade', 'deploy', 'upgrade_providers', 'generate_nginx_conf']
+__all__ = ['setup', 'upgrade', 'deploy', 'reload', 'upgrade_providers', 'generate_nginx_conf']
