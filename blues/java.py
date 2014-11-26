99a54a330982d2b49d4f49517567448543ce78ce
Jonas Lundberg <jonas@5monkeys.se>
2014-11-26 15:21:16 +0100

    Add Java blueprint

---
+++ blues/java.py
@@ -0,0 +1,32 @@
+from fabric.decorators import task
+
+from refabric.api import run, info
+from refabric.context_managers import sudo
+
+from . import debian
+
+__all__ = ['setup']
+
+
+@task
+def setup():
+    """
+    Install Java
+    """
+    install()
+
+
+def install():
+    with sudo():
+        lbs_release = debian.lbs_release()
+
+        if lbs_release == '12.04':
+            debian.add_apt_ppa('webupd8team/java')
+            debian.debconf_set_selections('shared/accepted-oracle-license-v1-1 select true',
+                                          'shared/accepted-oracle-license-v1-1 seen true')
+            package = 'oracle-java7-installer'
+        else:
+            package = 'java7-jdk'
+
+        info('Install Java 7 JDK')
+        debian.apt_get('install', package)
