d5ea5de8fb8c134178a218f91eb06cf42856e6dc
Hannes Ljungberg <hannes@5monkeys.se>
2014-11-27 10:25:01 +0100

    Add mongodb blueprint

---
+++ blues/mongodb.py
@@ -0,0 +1,42 @@
+from fabric.decorators import task
+
+from refabric.context_managers import sudo
+from refabric.contrib import blueprints
+
+from . import debian
+
+__all__ = ['start', 'stop', 'restart', 'setup', 'configure']
+
+
+blueprint = blueprints.get(__name__)
+
+start = debian.service_task('mongodb', 'start')
+stop = debian.service_task('mongodb', 'stop')
+restart = debian.service_task('mongodb', 'restart')
+
+
+@task
+def setup():
+    """
+    Install and configure mongodb
+    """
+    install()
+    configure()
+
+
+def install():
+    with sudo():
+        debian.apt_get('install', 'mongodb')
+
+
+@task
+def configure():
+    """
+    Configure mongodb
+    """
+    context = {
+        'bind': blueprint.get('bind', '127.0.0.1')
+    }
+    uploads = blueprint.upload('mongodb.conf', '/etc/mongodb.conf', context)
+    if uploads:
+        restart()
