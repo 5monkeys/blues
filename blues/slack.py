b4acdc3c57eb984a09a369b0fa8119d744af4a81
Johannes Löthberg <johannes@5monkeys.se>
2015-05-27 17:01:39 +0200

    [slack] Add support for notifying multiple channels
    Fixes #19

--- blues/slack.py
+++ blues/slack.py
@@ -9,31 +9,46 @@ Slack Blueprint
     settings:
       slack:
         endpoint: https://hooks.slack.com/...
-        #channel: "#deploy"
+        #channels:
+        #  - "#deploy"
         #username: deploybot
         #icon_emoji: ":rocket"
 
 """
 from refabric.contrib import blueprints
+from collections import OrderedDict
 import urllib2
 import json
 
 blueprint = blueprints.get(__name__)
 
 def notify(msg):
-    channel = blueprint.get('channel', '#deploy')
+    channels = blueprint.get('channels', ['#deploy'])
+    channel = blueprint.get('channel', None)
+
+    # If channel is specified, add it to channels, and then run it through an
+    # OrderedDict, removing any duplicates.
+    if channel:
+        channels.append(channel)
+        channels = list(OrderedDict.fromkeys(channels))
+
     username = blueprint.get('username', 'deploybot')
     icon_emoji = blueprint.get('icon_emoji', ':rocket:')
 
     endpoint = blueprint.get('endpoint')
     if not endpoint:
-        return None
+        return False
 
+    for channel in channels:
+        send_request(endpoint, channel, username, msg, icon_emoji)
+
+def send_request(endpoint, channel, username, msg, icon_emoji):
     data = json.dumps({
         "channel": channel,
         "username": username,
         "text": msg,
         "icon_emoji": icon_emoji,
     })
+
     req = urllib2.Request(endpoint, data, {'Content-Type': 'application/json'})
     urllib2.urlopen(req).close()
