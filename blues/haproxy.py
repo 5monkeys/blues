82c4200bb29fa584f1b19d7e4f28429183b12f4a
Johannes Löthberg <johannes@5monkeys.se>
2015-07-07 10:33:14 +0200

    haproxy: Add initial blueprint
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

---
+++ blues/haproxy.py
@@ -0,0 +1,44 @@
+from fabric.decorators import task
+
+from refabric.contrib import blueprints
+
+from . import debian
+
+__all__ = ['start', 'stop', 'restart', 'status', 'setup', 'configure']
+
+
+blueprint = blueprints.get(__name__)
+
+start = debian.service_task('haproxy', 'start')
+stop = debian.service_task('haproxy', 'stop')
+restart = debian.service_task('haproxy', 'restart')
+status = debian.service_task('haproxy', 'status')
+
+
+@task
+def setup():
+    """
+    Install and configure HAProxy
+    """
+    install()
+    configure()
+
+
+@task
+def install():
+    from refabric.context_managers import sudo
+    from .debian import add_apt_ppa, apt_get
+
+    with sudo():
+        add_apt_ppa('vbernat/haproxy-1.5', src=True)
+        apt_get('install', 'haproxy')
+
+
+@task
+def configure():
+    """
+    Render and upload haproxy.cfg
+    """
+    uploads = blueprint.upload('./', '/etc/haproxy/')
+    if uploads:
+        restart()
