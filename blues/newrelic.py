54915eb39fdfda395b712c64073cd1d5c61236fc
Philip Zalitis <philip@sportamore.se>
2015-12-08 12:41:51 +0100

    simple newrelic notification

--- blues/newrelic.py
+++ blues/newrelic.py
@@ -19,8 +19,13 @@ from refabric.api import run, info
 
 from refabric.context_managers import sudo
 from refabric.contrib import blueprints
+from fabric.context_managers import cd
+from .application.project import python_path
 
-from . import debian
+
+from . import debian, git
+
+import requests
 
 __all__ = ['start', 'stop', 'restart', 'setup', 'configure']
 
@@ -64,3 +69,34 @@ def configure():
         info('Adding license key to config')
         newrelic_key = blueprint.get('newrelic_key', None)
         run('nrsysmond-config --set license_key={}'.format(newrelic_key))
+
+
+def send_deploy_event():
+
+    newrelic_key = blueprint.get('newrelic_key', None)
+    app_name = blueprint.get('app_name', None)
+
+    if newrelic_key and app_name:
+        url = 'https://api.newrelic.com/deployments.xml'
+        headers = {'x-api-key': newrelic_key}
+        with cd(python_path()):
+            tag = run('git describe --tags')
+            commit_hash = run('git rev-parse HEAD')
+
+        deployer = git.get_local_commiter()
+
+        payload = {
+                'deployment[app_name]': app_name,
+                # 'application_id': '1234567',
+                'deployment[description]': tag,
+                'deployment[revision]': commit_hash,
+                # 'deployment[changelog]': changes,
+                'deployment[user]': deployer,
+            }
+
+        requests.post(url, data=payload, headers=headers)
+
+    else:
+        info('No key found')
+
+#http://gc-taylor.com/blog/2013/02/11/fabric-task-for-notifying-new-relic-code-deploy/#sthash.5AnhN3An.sfju
