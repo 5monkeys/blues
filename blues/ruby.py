a63d1f5973fd058b11854b3071dac888a1040f4b
Henrik Lindgren <henriklindgren@users.noreply.github.com>
2016-04-11 11:15:38 +0200

    ruby.py legacy support for gem install -v flags

--- blues/ruby.py
+++ blues/ruby.py
@@ -52,17 +52,34 @@ def install():
         debian.apt_get('install', 'ruby1.9.3')
 
     info('Installing Bundler')
-    gem('install', 'bundler')
+    install_gem('bundler')
 
 
 def install_gems():
+    """
+    Example with 4 kinds of installs supported (at the same time but on
+    different rows),
+    * no arguments,
+    * multiple gems without arguments,
+    * a gem with an argument,
+    * a gem with multiple arguments
+
+    gems:
+        gem2
+        gem4 gem5 gem6
+        gem3 -arg3s
+        gem1 -arg1 -arg2
+    """
     gems = blueprint.get('gems', [])
     if gems:
         info('Installing Gems')
-        gem('install', *gems)
+    # for older versions of gem, you can't install on a single row while
+    # specifying version so we need to loop the install command accordingly.
+    for gem in gems:
+        install_gem(gem)
 
 
-def gem(command, *options):
-    info('Running gem {}', command)
+def install_gem(*options):
+    info('Running gem install')
     with sudo():
-        run('gem {} {} --no-ri --no-rdoc'.format(command, ' '.join(options)))
+        run('gem install {} --no-ri --no-rdoc'.format(' '.join(options)))
