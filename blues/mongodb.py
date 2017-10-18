62f07b0fba4e28cbf3bab24b12cd23e7f0eebe77
Andrei Fokau <andrei@5monkeys.se>
2017-10-18 18:11:37 +0200

    mongodb: Fix setting up users for versions before 2.6.

--- blues/mongodb.py
+++ blues/mongodb.py
@@ -12,6 +12,7 @@ MongoDB Blueprint
     settings:
       mongodb:
         # bind: 0.0.0.0  # Set the bind address specifically (Default: 127.0.0.1)
+        replSet: webscale
         keyfile: 'mongodb-keyfile'
         admin:
           user: admin          # Superuser username
@@ -60,19 +61,26 @@ def install():
         debian.apt_get('install', 'mongodb')
 
 
+def get_version():
+    r = _client_exec('db.version()', auth=False)
+    return map(int, r.split('.'))
+
+
+def upload_conf(auth=None):
+    context = {
+        'bind': blueprint.get('bind', '127.0.0.1'),
+        'auth': blueprint.get('auth', True) if auth is None else auth,
+    }
+    return blueprint.upload('mongodb.conf', '/etc/mongodb.conf', context)
+
+
 @task
-def configure(auth=None):
+def configure():
     """
     Configure mongodb
     """
-    context = {
-        'bind': blueprint.get('bind', '127.0.0.1'),
-        'auth': blueprint.get('auth', True),
-    }
-    if auth is not None:
-        context['auth'] = auth
     uploads = [
-        blueprint.upload('mongodb.conf', '/etc/mongodb.conf', context)
+        upload_conf()
     ]
     keyfile = blueprint.get('keyfile')
     if keyfile is not None:
@@ -89,6 +97,11 @@ def setup_admin():
     """
     Creates a superuser account.
     """
+    if not blueprint.get('auth', True) or blueprint.get('slave'):
+        return
+
+    upload_conf(auth=False) and restart()
+
     admin = blueprint.get('admin', {})
     roles = [
         {'role': 'userAdminAnyDatabase', 'db': 'admin'},
@@ -100,12 +113,16 @@ def setup_admin():
     # Restart mongod without the auth flag
     _ensure_user('admin', admin['user'], admin['password'], roles, auth=False)
 
+    upload_conf(auth=True) and restart()
+
 
 @task
 def setup_schemas():
     """
     Creates database schemas and grant user privileges.
     """
+    if blueprint.get('slave'):
+        return
     schemas = blueprint.get('schemas', {})
     for schema, config in schemas.iteritems():
         info('Setting up schema {}', schema)
@@ -113,8 +130,30 @@ def setup_schemas():
         _ensure_user(schema, config['user'], config['password'], roles)
 
 
+def _add_user(schema, user, password, roles, auth=True):
+    roles = [r['role'] for r in roles]
+    roles = json.dumps(roles).replace('"', '\'')
+    _client_exec("""
+        use %(schema)s;
+        db.removeUser('%(user)s');
+        db.addUser({
+            user: '%(user)s',
+            pwd: '%(password)s',
+            roles: %(roles)s
+        })
+    """, user=user, password=password, roles=roles, auth=auth, schema=schema)
+
+
 def _ensure_user(schema, user, password, roles, auth=True):
     info('Creating/updating user {}', user)
+
+    version = get_version()
+    if version < (2, 4):
+        raise NotImplementedError
+
+    elif version < (2, 6):
+        return _add_user(schema, user, password, roles, auth=auth)
+
     roles = json.dumps(roles).replace('"', '\'')
     r = _client_exec("""
         use %(schema)s;
